ef075611ad01fbcaa36e2fbf72467da761cce0d3
Varad Kanekar <kanekarvarad@gmail.com>
2024-07-27 17:14:10 +0530

    Add Scanner and Random objects for input and random number generation

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,2 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        // Add Scanner and Random objects for input and random number generation
+        boolean playAgain;
+        int totalRounds = 0;
+        int totalAttempts = 0;
+    }
+}
